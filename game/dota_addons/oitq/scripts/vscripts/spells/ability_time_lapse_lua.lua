fdd52324bf54c6e81f302b47d10efd5787f3df2d
Frixs <tomkveton@seznam.cz>
2016-07-19 15:21:50 +0200

    Ability Time-Lapse: Improved debuff

--- game/dota_addons/oitq/scripts/vscripts/spells/ability_time_lapse_lua.lua
+++ game/dota_addons/oitq/scripts/vscripts/spells/ability_time_lapse_lua.lua
@@ -1,12 +1,20 @@
 function OnDestroy( keys )
-	local caster 		  = keys.caster
-	local ability 		  = keys.ability
-	local debuff_duration = keys.DebuffDuration
+	local caster 		  			 	   = keys.caster
+	local ability 		  			 	   = keys.ability
+	local ability_level 			 	   = ability:GetLevel() - 1
+	local ability_cooldown			 	   = ability:GetCooldownTimeRemaining()
+	local debuff_duration 			 	   = ability:GetLevelSpecialValueFor("debuff_duration", ability_level)
+	local debuff_additional_cooldown       = ability:GetLevelSpecialValueFor("debuff_additional_cooldown", ability_level)
 
 	-- apply debuff if caster didn't use any ability or item
 	if not caster:HasModifier("modifier_ability_time_lapse_debuff_controller") then
+		-- apply debuff
 		ability:ApplyDataDrivenModifier(caster, caster, "modifier_ability_time_lapse_debuff", {duration = debuff_duration})
 
+		-- reset cooldown + debuff additional cooldown
+		ability:StartCooldown( (ability_cooldown + debuff_additional_cooldown) )
+
+		-- appearance
 		caster:EmitSound("DOTA_CustomAbility.TimeLapse.End")
 		caster.EndParticle = ParticleManager:CreateParticle("particles/units/heroes/hero_abaddon/abaddon_borrowed_time_f.vpcf", PATTACH_ABSORIGIN_FOLLOW, caster)
 	end
