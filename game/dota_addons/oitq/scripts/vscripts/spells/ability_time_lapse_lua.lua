b0728989354427b2f517b7df49d633ecd0cd403b
Frixs <tomkveton@seznam.cz>
2016-07-19 14:22:55 +0200

    Ability Time-Lapse: Added particles, sounds, tooltips

--- game/dota_addons/oitq/scripts/vscripts/spells/ability_time_lapse_lua.lua
+++ game/dota_addons/oitq/scripts/vscripts/spells/ability_time_lapse_lua.lua
@@ -6,6 +6,9 @@ function OnDestroy( keys )
 	-- apply debuff if caster didn't use any ability or item
 	if not caster:HasModifier("modifier_ability_time_lapse_debuff_controller") then
 		ability:ApplyDataDrivenModifier(caster, caster, "modifier_ability_time_lapse_debuff", {duration = debuff_duration})
+
+		caster:EmitSound("DOTA_CustomAbility.TimeLapse.End")
+		caster.EndParticle = ParticleManager:CreateParticle("particles/units/heroes/hero_abaddon/abaddon_borrowed_time_f.vpcf", PATTACH_ABSORIGIN_FOLLOW, caster)
 	end
 	
 	-- remove debuff controller
