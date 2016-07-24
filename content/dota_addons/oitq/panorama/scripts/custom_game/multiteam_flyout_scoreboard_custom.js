815358b51bf7bb2df7427a4d4173e7d0b8a5fcec
Frixs <tomkveton@seznam.cz>
2016-07-24 10:52:13 +0200

    end screen scoreboard complete

--- content/dota_addons/oitq/panorama/scripts/custom_game/multiteam_flyout_scoreboard_custom.js
+++ content/dota_addons/oitq/panorama/scripts/custom_game/multiteam_flyout_scoreboard_custom.js
@@ -17,9 +17,24 @@ function GetScore()
             if (!teamScore.hasOwnProperty(key)) continue;
             var value = teamScore[key];
             
+            // flyout + endscreen
             $("#_dynamic_team_"+key+"").GetChild(0).GetChild(2).GetChild(0).text = value;
+            //endscreen
+            $("#_dynamic_team_"+key+"").GetChild(0).GetChild(3).GetChild(0).text = GetTeamKills(parseInt(key));
         }
     }
+}
+
+///-----CUSTOM-----///
+function GetTeamKills( teamID ) {
+	var playersOnTeam = Game.GetPlayerIDsOnTeam( teamID )
+	var killsForTeam = 0;
+
+	for (var i = 0; i < playersOnTeam.length; i++) {
+		killsForTeam+=Players.GetKills( playersOnTeam[i] );
+	}
+
+	return killsForTeam;
 }
 
 (function()
