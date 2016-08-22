6316733caacc8cc2946ebe00789f7b14befbef80
Frixs <tomkveton@seznam.cz>
2016-08-22 13:15:50 +0200

    Ability FIX: IceBlock - movement issues after death

--- game/dota_addons/oitq/scripts/vscripts/spells/ice_block_spell_lua.lua
+++ game/dota_addons/oitq/scripts/vscripts/spells/ice_block_spell_lua.lua
@@ -1,14 +1,3 @@
-function immobility( event )
-	if IsServer() then
-		local caster = event.caster
-		if caster == nil then
-		  return
-		end
-
-	    caster:SetBaseMoveSpeed( 100 )
-	end
-end
-
 function set_invul_on( event )
 	if IsServer() then
 		local caster = event.caster
@@ -30,21 +19,5 @@ function set_invul_off( event )
 
 	    caster:SetPhysicalArmorBaseValue( 0 )
 	    caster:SetBaseMagicalResistanceValue( 0 )
-	end
-end
-
-function increase_move_speed( event )
-	if IsServer() then
-		local caster = event.caster
-		local ability = event.ability
-		
-		if caster == nil then
-		  return
-		end
-
-	    local actual_move_speed = caster:GetBaseMoveSpeed()
-	    local pick_up = ability:GetSpecialValueFor("slow_steps")
-	    
-	    caster:SetBaseMoveSpeed( actual_move_speed + pick_up )
 	end
 end
